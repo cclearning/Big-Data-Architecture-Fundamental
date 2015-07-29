52ea66bef81965076d1764fda32ddb2075c74dd3
root <yy@cclearning.asia>
2015-07-29 21:51:15 +0800

    add more docs

---
+++ spark_pi.sh
@@ -0,0 +1,8 @@
+./bin/spark-submit --class org.apache.spark.examples.SparkPi \
+    --master yarn-cluster \
+    --num-executors 3 \
+    --driver-memory 4g \
+    --executor-memory 2g \
+    --executor-cores 1 \
+    lib/spark-examples*.jar \
+    10
